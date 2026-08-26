4400bcd6f07bf7b8e517b2eb5c2f443076230e32
Leon <leon@example.com>
2026-08-26 21:26:38 +0000

    Tägliches Foto von Google Drive einbinden

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -7,3 +7,5 @@ window.__FIREBASE_CONFIG__ = {
   appId: "1:643165169880:web:f4c5c3173a01fc75f50fad",
   measurementId: "G-SVK8ERZ8PC"
 };
+window.__DRIVE_API_KEY__ = "AIzaSyDtXliseeJsFuedTIaFQxwXwHIwDo59k5I";
+window.__DRIVE_ORDNER_ID__ = "1JQEwy1WaJi0Bn3GblfAa-FH3sPVq3Rg9";
