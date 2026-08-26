5fab3e3aaf55476d5d212637066b1cc27d92b6bd
Leon <leon@example.com>
2026-08-26 18:47:34 +0000

    Echte Firebase-Zugangsdaten fest hinterlegt

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyCj8vSVvdpFW4XxoDHxaNycbhb5-CbrbMQ",
+  authDomain: "osnacountdown.firebaseapp.com",
+  projectId: "osnacountdown",
+  storageBucket: "osnacountdown.firebasestorage.app",
+  messagingSenderId: "643165169880",
+  appId: "1:643165169880:web:f4c5c3173a01fc75f50fad",
+  measurementId: "G-SVK8ERZ8PC"
+};
